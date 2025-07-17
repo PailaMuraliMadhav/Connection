bba1454297f1abb04d6ea8aeaaed5ad652bb6c69
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-07-17 10:18:16 +0530

    added Title

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/users`);
     <div className="min-h-screen bg-gradient-to-br from-red-100 to-white p-6">
       <div className="max-w-4xl mx-auto text-center">
         <h1 className="text-4xl font-bold text-red-700 mb-6 drop-shadow">
-          HAHAHAHAHAHAHHAHA
+          Users Dashboard
         </h1>
 
         <p className="text-gray-700 text-lg mb-4">
